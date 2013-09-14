cac612d4f0cee0c6c14abf8991acd40e66e3174a
Richard Nixon <richard2718@gmail.com>
2013-09-14 14:34:55 +0200

    added run script code

--- _runall.R
+++ _runall.R
@@ -4,6 +4,16 @@ root <- "/Users/Richard/Documents/R/horse"
 
 
 #####################################################################
-
 source("_setup.R")
+source(file.path(gv$script, "data_manip.R"))
 
+# Run scripts
+
+setwd(gv$scripts)
+spin("exploratory_plots.R")
+
+unlink("figure", recursive = TRUE)
+unlink("*.Rmd")
+unlink("*.md")
+
+setwd(gv$root)
